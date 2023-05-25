9d9eb629e2f86445ff83754a3c4ffbb521a74631
Your Name <chetandasa2814@gmail.com>
2023-05-25 14:48:48 +0530

    ..

--- proTwo/appTwo/views.py
+++ proTwo/appTwo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+# sdcs
 
 def index(request):
     return HttpResponse("<em>My Second Project</em>")
